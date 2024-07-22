d6aa727dda0f0b0aa4a5011bca635f89e86b63ef
Lumina107 <rhys@globalblders.com>
2024-07-22 18:09:28 +0100

    Update Preloader.jsx

--- src/Preloader.jsx
+++ src/Preloader.jsx
@@ -1,19 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import Preload from "./assets/preloader.mp4";
 
 export default function Preloader() {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     window.onload = () => {
       window.scrollTo(0, 0);
       const preloader = document.querySelector(".preloader");
       setTimeout(() => {
         preloader.remove();
+        setLoading(false);
       }, 2000);
     };
   });
   return (
-    <section className="preloader">
+    <section className={`${loading ? "preloader" : "hidden"}`}>
       <ReactPlayer
         url={Preload}
         controls={false}
